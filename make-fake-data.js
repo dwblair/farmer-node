b0dfc96044ed9f7f06f4e8406d633c9ec78ce3ed
Don Blair <donblair@gmail.com>
2017-11-29 12:51:06 -0500

    update

--- make-fake-data.js
+++ make-fake-data.js
@@ -2,36 +2,32 @@
 
 const fs = require('fs')
 
-//let rows = ["name,value"]
-
-//rows.push(`thing1,${Math.round(Math.random()*1000)}`)
-//rows.push(`thing2,${Math.round(Math.random()*1000)}`)
-
-//const output = rows.join("\n")
 var timeStamp = Math.floor(Date.now() / 1000);
 var x=Math.round(Math.random()*1000);
 var y=Math.round(Math.random()*1000);
 
 if(fs.existsSync('data.txt')) {
 
+// data file exists; append data to it
+
 let rows=[]
 rows.push(`${timeStamp},${x},${y}`)
 let output = rows.join("\n")
 output=output.concat("\n")
-
 fs.appendFileSync('data.txt',output)
 
 }
+
 else {
 
+// data file doesn't exist; need to create the file and add header to top
+
 let rows=["timeStamp,x,y"]
-
 rows.push(`${timeStamp},${x},${y}`)
-
 let output=rows.join("\n")
 output=output.concat("\n")
-
 fs.writeFileSync('data.txt', output)
+
 }
 
 

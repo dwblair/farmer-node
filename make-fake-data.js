67ae754d26ba893eef1edfd0d5ba25bab01070e6
Don Blair <donblair@gmail.com>
2017-11-29 12:49:01 -0500

    update

--- make-fake-data.js
+++ make-fake-data.js
@@ -2,10 +2,36 @@
 
 const fs = require('fs')
 
-let rows = ["name,value"]
+//let rows = ["name,value"]
 
-rows.push(`thing1,${Math.round(Math.random()*1000)}`)
-rows.push(`thing2,${Math.round(Math.random()*1000)}`)
+//rows.push(`thing1,${Math.round(Math.random()*1000)}`)
+//rows.push(`thing2,${Math.round(Math.random()*1000)}`)
 
-const output = rows.join("\n")
+//const output = rows.join("\n")
+var timeStamp = Math.floor(Date.now() / 1000);
+var x=Math.round(Math.random()*1000);
+var y=Math.round(Math.random()*1000);
+
+if(fs.existsSync('data.txt')) {
+
+let rows=[]
+rows.push(`${timeStamp},${x},${y}`)
+let output = rows.join("\n")
+output=output.concat("\n")
+
+fs.appendFileSync('data.txt',output)
+
+}
+else {
+
+let rows=["timeStamp,x,y"]
+
+rows.push(`${timeStamp},${x},${y}`)
+
+let output=rows.join("\n")
+output=output.concat("\n")
+
 fs.writeFileSync('data.txt', output)
+}
+
+
---
+++ make-fake-data_original.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+
+const fs = require('fs')
+
+let rows = ["name,value"]
+
+rows.push(`thing1,${Math.round(Math.random()*1000)}`)
+rows.push(`thing2,${Math.round(Math.random()*1000)}`)
+
+const output = rows.join("\n")
+
+if(fs.existsSync('data.txt')) {
+    console.log('whoop!');
+}
+
+fs.writeFileSync('data.txt', output)
